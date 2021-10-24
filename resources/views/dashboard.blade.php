bcae545c79e4480761fddb9c442cdb088cf89e3e
Wasim Ramzan <wasimramzan04@gmail.com>
2021-10-24 15:58:09 +0100

    UI updates to the dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,33 +1,33 @@
 <x-app-layout>
 		<div class="relative w-full antialiased h-screen overflow-y-none md:flex md:flex-wrap">
-			<aside class="w-full lg:w-24 p-2 px-4">
+			<aside class="bg-gray-200 w-full lg:w-24 p-2 px-4">
 				<ul class="flex items-center justify-center lg:h-full lg:flex-col">
 					<li class="mb-2">
-						<button class="p-5 px-6 bg-green-300 rounded hover:bg-green-200 shadow-md">
-							<i class="fas fa-list text-gray-700">
-								<span class="sr-only">Feed</span>
-							</i>
+						<button class="p-3 px-3 bg-green-500 rounded hover:bg-green-200 shadow-md">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                              </svg>
 						</button>
 					</li>
 					<li class="mb-2">
-						<button class="p-5 px-6 bg-green-300 rounded hover:bg-green-100 hover:shadow-md">
-							<i class="fas fa-user text-black-700">
-								<span class="sr-only">My Profile</span>
-							</i>
+						<button class="p-3 px-3 bg-green-500 rounded hover:bg-green-100 hover:shadow-md">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                              </svg>
 						</button>
 					</li>
 					<li class="mb-2">
-						<button class="p-5 px-6 bg-green-300 rounded hover:bg-green-100 hover:shadow-md">
-							<i class="fas fa-calendar text-gray-700">
-								<span class="sr-only">Member List</span>
-							</i>
+						<button class="p-3 px-3 bg-green-500 rounded hover:bg-green-100 hover:shadow-md">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                              </svg>
 						</button>
 					</li>
 					<li class="mb-2">
-						<button class="p-5 px-6 bg-green-300 rounded hover:bg-green-100 hover:shadow-md">
-							<i class="fas fa-trophy text-gray-700">
-								<span class="sr-only">Optional</span>
-							</i>
+						<button class="p-3 px-3 bg-green-500 rounded hover:bg-green-100 hover:shadow-md">
+							<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                              </svg>
 						</button>
 					</li>
 				</ul>
